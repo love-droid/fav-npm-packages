88265cc800823b2648d2a283a7b90fe543efd6ab
luv <love.vig12@gmail.com>
2023-11-29 02:11:37 +0530

    Add favourites page structure

--- src/Pages/AddFavourite.js
+++ src/Pages/AddFavourite.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Button from '../Components/Button';
 
 function AddFavourite() {
-    
+  const navigate = useNavigate();
+
   return (
     <div className='h-[80vh]'>
-        <input>
-        
-        </input>
+      {/* Search bar */}
+      <div className="relative w-[80%] mx-auto mt-8">
+        <div className="absolute top-0 left-0 flex items-center px-3 py-2">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="h-5 w-5 text-gray-500" />
+        </div>
+        <input
+          type="search"
+          placeholder="Search"
+          className="w-full rounded-md pl-10 pr-4 py-2 bg-gray-200 border-gray-300 focus:border-indigo-500 focus:outline-none"
+        />
+      </div>
 
+      {/* Radio group template */}
+      <div className="mt-8">
+        <label className="inline-flex items-center">
+          <input type="radio" className="form-radio" name="radio" />
+          <span className="ml-2">Radio Button</span>
+        </label>
+      </div>
+
+      {/* Button */}
+      <Button className="mt-8" />
     </div>
-  )
+  );
 }
 
-export default AddFavourite
+export default AddFavourite;
